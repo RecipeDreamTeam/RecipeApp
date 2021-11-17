e77215844fdd205e0eadd52dd30f5fd00bb45eb2
Lauren Work <workla@oregonstate.edu>
2021-11-16 19:58:15 -0800

    Add skeleton profile grid - need to change out logic to get favorited recipes and not all recipes

---
+++ RecipeApp/RecipeApp/ProfileRecipeCell.swift
@@ -0,0 +1,16 @@
+//
+//  ProfileRecipeCell.swift
+//  RecipeApp
+//
+//  Created by Lauren Work on 11/16/21.
+//
+
+import UIKit
+
+class ProfileRecipeCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var profileRecipeImageView: UIImageView!
+    
+    @IBOutlet weak var profileRecipeTitle: UILabel!
+}
--- RecipeApp/RecipeApp/ProfileViewController.swift
+++ RecipeApp/RecipeApp/ProfileViewController.swift
@@ -1,36 +1,36 @@
 //
-//  AllRecipeGridViewController.swift
+//  ProfileViewController.swift
 //  RecipeApp
 //
-//  Created by Lauren Work on 11/5/21.
+//  Created by Lauren Work on 11/16/21.
 //
 
 import UIKit
-import AlamofireImage
 import Parse
+import AlamofireImage
 
-class AllRecipeGridViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+class ProfileViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     var recipes = [PFObject]()
     let myRefreshControl = UIRefreshControl()
-    
-    @IBOutlet weak var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         collectionView.delegate = self
         collectionView.dataSource = self
-        
+
         let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
         layout.minimumLineSpacing = 8
         layout.minimumInteritemSpacing = 8
-        
+
         let width = (view.frame.size.width - (layout.minimumInteritemSpacing)) / 2
-        
-        
+
+
         layout.itemSize = CGSize(width: width, height: width*1.2)
-        
+
         myRefreshControl.addTarget(self, action: #selector(loadRecipes), for: .valueChanged)
         collectionView.refreshControl = myRefreshControl
         
@@ -42,14 +42,13 @@ class AllRecipeGridViewController: UIViewController, UICollectionViewDataSource,
         loadRecipes()
 
     }
-    
+
     @objc func loadRecipes(){
+        //rewrite query for only user liked recipes when favorites exist in parse
         let query = PFQuery(className: "Recipes")
-        //add author back in when login complete
-        //query.includeKeys(["author"])
         query.limit = 20
         query.order(byDescending: "createdAt")
-        
+
         query.findObjectsInBackground { (recipes, error) in
             if recipes != nil {
                 self.recipes = recipes!
@@ -62,24 +61,24 @@ class AllRecipeGridViewController: UIViewController, UICollectionViewDataSource,
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return recipes.count
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "RecipeCell", for: indexPath) as! RecipeCell
-        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ProfileRecipeCell", for: indexPath) as! ProfileRecipeCell
+
         //need to switch out final config when we have recipe PFObjects
         let recipe = recipes[indexPath.item]
         let title = recipe["title"] as? String
-        
+
         let imageFile = recipe["image"] as! PFFileObject
         let urlString = imageFile.url!
         let url = URL(string: urlString)!
-        
-        cell.recipeTitleLabel.text = title
-        cell.recipeImageView.af.setImage(withURL: url)
-        
+
+        cell.profileRecipeTitle.text = title
+        cell.profileRecipeImageView.af.setImage(withURL: url)
+
         return cell
     }
-    
+
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
 
         if (sender is UICollectionViewCell) {
@@ -94,8 +93,6 @@ class AllRecipeGridViewController: UIViewController, UICollectionViewDataSource,
         } else {
             super.prepare(for: segue, sender: sender)
         }
-
     }
 
 }
-    
