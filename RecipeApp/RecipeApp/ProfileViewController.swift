ec2b31c3c033f1c88bef2c0e729882b669fad135
soman24 <khansom@oregonstate.edu>
2021-11-17 18:38:25 -0600

    logout button

--- RecipeApp/RecipeApp/ProfileViewController.swift
+++ RecipeApp/RecipeApp/ProfileViewController.swift
@@ -95,4 +95,19 @@ class ProfileViewController: UIViewController, UICollectionViewDataSource, UICol
         }
     }
 
+    
+    @IBAction func onLogoutButton(_ sender: Any) {
+        PFUser.logOut()
+        
+        let main = UIStoryboard(name: "Main", bundle: nil)
+        let loginViewController =
+            main.instantiateViewController(withIdentifier: "LoginViewController")
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene, let
+                delegate = windowScene.delegate as? SceneDelegate else { return }
+        
+        delegate.window?.rootViewController = loginViewController
+    }
+
+    
+    
 }
