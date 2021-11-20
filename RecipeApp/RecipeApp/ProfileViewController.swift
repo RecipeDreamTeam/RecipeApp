c038f22a500a69d47b77cc6817df17d4f5a0fd85
Lauren Work <workla@oregonstate.edu>
2021-11-19 16:48:40 -0800

    Change profile view to only show favorited recipes

--- RecipeApp/RecipeApp/ProfileViewController.swift
+++ RecipeApp/RecipeApp/ProfileViewController.swift
@@ -13,7 +13,7 @@ class ProfileViewController: UIViewController, UICollectionViewDataSource, UICol
 
     @IBOutlet weak var collectionView: UICollectionView!
     
-    var recipes = [PFObject]()
+    var favorites = [PFObject]()
     let myRefreshControl = UIRefreshControl()
     
     override func viewDidLoad() {
@@ -45,13 +45,25 @@ class ProfileViewController: UIViewController, UICollectionViewDataSource, UICol
 
     @objc func loadRecipes(){
         //rewrite query for only user liked recipes when favorites exist in parse
-        let query = PFQuery(className: "Recipes")
-        query.limit = 20
-        query.order(byDescending: "createdAt")
-
-        query.findObjectsInBackground { (recipes, error) in
-            if recipes != nil {
-                self.recipes = recipes!
+//        let query = PFQuery(className: "Recipes")
+//        query.limit = 20
+//        query.order(byDescending: "createdAt")
+//
+//        query.findObjectsInBackground { (recipes, error) in
+//            if recipes != nil {
+//                self.recipes = recipes!
+//                self.collectionView.reloadData()
+//                self.myRefreshControl.endRefreshing()
+//            }
+//        }
+        
+        let query2 = PFQuery(className: "Favorites")
+        query2.whereKey("user", equalTo: PFUser.current())
+        query2.includeKeys(["recipe"])
+        query2.order(byDescending: "createdAt")
+        query2.findObjectsInBackground { favorites, error in
+            if favorites != nil {
+                self.favorites = favorites!
                 self.collectionView.reloadData()
                 self.myRefreshControl.endRefreshing()
             }
@@ -59,14 +71,15 @@ class ProfileViewController: UIViewController, UICollectionViewDataSource, UICol
     }
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return recipes.count
+        return favorites.count
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ProfileRecipeCell", for: indexPath) as! ProfileRecipeCell
 
         //need to switch out final config when we have recipe PFObjects
-        let recipe = recipes[indexPath.item]
+        let favorite = favorites[indexPath.item]
+        let recipe = favorite["recipe"] as! PFObject
         let title = recipe["title"] as? String
 
         let imageFile = recipe["image"] as! PFFileObject
@@ -86,7 +99,8 @@ class ProfileViewController: UIViewController, UICollectionViewDataSource, UICol
             //find movie info
             let cell = sender as! UICollectionViewCell
             let indexPath = collectionView.indexPath(for: cell)!
-            let recipe = recipes[indexPath.item]
+            let favorite = favorites[indexPath.item]
+            let recipe = favorite["recipe"] as! PFObject
             //pass movie to controller
             let detailViewController = segue.destination as! RecipeDetailsViewController
             detailViewController.recipe = recipe
